32b4c6d43b7fcdd219955082e61430329b68b53b
Latif Pala <latifpala@gmail.com>
2020-05-14 20:23:38 +0530

    Bid New Field

--- template-js/modal-bid.php
+++ template-js/modal-bid.php
@@ -18,6 +18,8 @@ $commission          = ae_get_option( 'commission', 0 );
             </div>
             <div class="modal-body">
                 <form role="form" id="bid_form" class="bid-form fre-modal-form">
+                    <?php 
+                    /* 
                     <div class="fre-input-field">
                         <label class="fre-field-title" for="bid_budget"><?php _e( 'Your Bid', ET_DOMAIN ); ?></label>
                         <div class="fre-project-budget">
@@ -36,12 +38,20 @@ $commission          = ae_get_option( 'commission', 0 );
 								printf( __( "<p class='bid-commission-fee'>Commission fee: <b>%s</b></p>", ET_DOMAIN ), $commission_fee );
 							}
 						} ?>
+                    </div> */ ?>
+                    <div class="fre-input-field">
+                        <label class="fre-field-title" for="bid_budget"><?php _e( 'Preferred Method of Contact.', ET_DOMAIN ); ?></label>
+                        <select name="preferred_contact_method">
+                            <option value="<?php _e('Email', ET_DOMAIN); ?>"><?php _e('Email', ET_DOMAIN); ?></option>
+                            <option value="<?php _e('Phone', ET_DOMAIN); ?>"><?php _e('Phone', ET_DOMAIN); ?></option>
+                            <option value="<?php _e('Private Message', ET_DOMAIN); ?>"><?php _e('Private Message', ET_DOMAIN); ?></option>
+                        </select>
                     </div>
-                    
+                    <?php /*
                     <div class="fre-input-field no-margin-bottom">
                         <label class="fre-field-title"  for="post_content"><?php _e( 'Add Notes', ET_DOMAIN ); ?></label>
                         <textarea id="bid_content" name="bid_content"></textarea>
-                    </div>
+                    </div> */ ?>
                     <input type="hidden" name="post_parent" value="<?php the_ID(); ?>"/>
                     <input type="hidden" name="method" value="create"/>
                     <input type="hidden" name="action" value="ae-sync-bid"/>
