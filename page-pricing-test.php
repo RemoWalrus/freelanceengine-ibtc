6715f34aa9b5c2c919a48c3fb587d69d891fd976
Remo Walrus <36936130+RemoWalrus@users.noreply.github.com>
2020-06-02 14:33:21 -0400

    Pricing

--- page-pricing-test.php
+++ page-pricing-test.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Template Name: Pricing Page
+ * Template Name: Pricing ARTIST
  */
 
 get_header();
@@ -34,69 +34,112 @@ if(  is_user_logged_in() ) {
 }
 
 if( $show_project_pack || $show_bid_pack ){ ?>
-	<div class="fre-service">
-<div class="container">
+<div class="fre-service">
+  <div class="container">
 			<h2 id="title_service">
-				Select a plan to apply to projects			</h2>
+				Select an Artist Plan</h2>
 			<div class="fre-service-content">
-                <p>Our platform solely caters to an Artist’s creative appetite to find jobs, connect, and be inspired.</p>
+                <p style="text-align: center; font-size: 1.3em;">Our platform solely caters to an Artist’s creative appetite to find jobs, connect, and be inspired.</p>
                 
 	<div class="row">
 		<div class="col-md-1 hidden-sm"></div>
-		<div class="col-md-10">
+		<div class="col-md-12">
 			<div class="row fre-service-package-list">
 					    		<div class="col-md-3 col-sm-6">
 									<div class="fre-service-pricing">
 										<div class="service-price">
-											<h2>FREE</h2> 										</div>
+											<h2>FREE</h2> 	
+                                        </div>
 										<div class="service-info">
-											<h3>Basic Account</h3>
+											<h3>Basic </h3>
 											<p></p><p>Try us out for free and see what we’re about</p>
+                                        
 <p></p>
 										</div>
-																				<a class="fre-service-btn primary-color-hover" href=""<?php echo et_get_page_link('register', array("role"=>'freelancer')); ?>">Sign Up</a>
+												<a class="fre-btn primary-bg-color" data-hover="Sign Up" href="<?php echo et_get_page_link('register', array("role"=>'freelancer')); ?>"><?php _e('<span>Sign Up</span>', ET_DOMAIN);?></a>
+                                        <div class="service-info">
+                                        <ul>
+                                                <li>No Commitments</li>
+                                                <li>Create a profile and portfolio</li>
+                                                <li>Browse competitive beauty jobs</li>
+                                                <li>Connect and learn with your peers in the Artists’ Lounge </li>
+                                         </ul> 
+                                        </div>
+										
+                                      
 									</div>
 								</div>
+                                <div class="col-md-3 col-sm-6">
                 	            <div class="fre-service-pricing">
 										<div class="service-price">
-											<h2><sup>$</sup>35.00</h2> 										</div>
+											<h2><sup>$</sup>35<sub style="font-size:.3em; ">/month</sub></h2> 										</div>
 										<div class="service-info">
-											<h3>Monthy</h3>
+											<h3>Artist Light</h3>
 											<p></p><p>Pay as you go Month to Month</p>
 <p></p>
 										</div>
-																				<a class="fre-service-btn primary-color-hover" href="https://inbetweenthechair.com/stage/upgrade-account/?pack_id=640">Purchase</a>
+												<a class="fre-btn primary-bg-color" href="<?php echo get_site_url(); ?>/upgrade-account/?pack_id=640">Purchase</a>
+                                        <div class="service-info">
+                                        <ul>
+                                            <li>No Commitments</li>
+                                            <li>Create a profile and portfolio</li>
+                                            <li>Browse competitive beauty jobs</li>
+                                            <li>Apply to unlimited beauty jobs</li>
+                                            <li>Connect and learn with your peers in the Artists’ Lounge </li>
+                                         </ul> 
+                                        </div>
 								</div>
 								</div>
                                 <div class="col-md-3 col-sm-6">
 									<div class="fre-service-pricing">
 										<div class="service-price">
-											<h2><sup>$</sup>150.00</h2> 										</div>
+											<h2><sup>$</sup>150<sub style="font-size:.3em; ">/month</sub></h2> 										</div>
 										<div class="service-info">
-											<h3>6 Month</h3>
-											<p></p><p>$25 a month subscription paid in one payment of $150 every 6 months</p>
+											<h3>Artist Standard</h3>
+											<p></p><p>28% Savings Six month subscription paid in one payment of $150 every 6 months</p>
 <p></p>
 										</div>
-																				<a class="fre-service-btn primary-color-hover" href="https://inbetweenthechair.com/stage/upgrade-account/?pack_id=641">Purchase</a>
+												<a class="fre-btn primary-bg-color" href="<?php echo get_site_url(); ?>/upgrade-account/?pack_id=641">Purchase</a>
+                                        <div class="service-info">
+                                        <ul>
+                                            <li>No Commitments</li>
+                                            <li>Create a profile and portfolio</li>
+                                            <li>Browse competitive beauty jobs</li>
+                                            <li>Apply to unlimited beauty jobs</li>
+                                            <li>Connect and learn with your peers in the Artists’ Lounge </li>
+                                         </ul> 
+                                        </div>
 									</div>
 								</div>
                                  <div class="col-md-3 col-sm-6">
 									<div class="fre-service-pricing">
 										<div class="service-price">
-											<h2><sup>$</sup>180.00</h2> 										</div>
+											<h2><sup>$</sup>180<sub style="font-size:.3em; ">/month</sub></h2> 										</div>
 										<div class="service-info">
-											<h3>Yearly</h3>
-											<p></p><p>$15 a month for a year with a subscription paid in 1 payment of $180 every year</p>
+											<h3>Artist Premium</h3>
+											<p></p><p>57% Savings Annual subscription paid in one payment of $180 every year</p>
 <p></p>
 										</div>
-																				<a class="fre-service-btn primary-color-hover" href="https://inbetweenthechair.com/stage/upgrade-account/?pack_id=642">Purchase</a>
+												
+                                            <a class="fre-btn primary-bg-color" href="h<?php echo get_site_url(); ?>/upgrade-account/?pack_id=642">Purchase</a>
+                                        <div class="service-info">
+                                         <ul>
+                                            <li>No Commitments</li>
+                                            <li>Create a profile and portfolio</li>
+                                            <li>Browse competitive beauty jobs</li>
+                                            <li>Apply to unlimited beauty jobs</li>
+                                            <li>Connect and learn with your peers in the Artists’ Lounge </li>
+                                         </ul> 
+                                        </div>
 									</div>
 								</div> 			</div>
 		</div>
 		<div class="col-md-1 hidden-sm"></div>
 	</div>
-</div>		</div>
-	</div>
+
+    </div>		
+  </div>
+</div>
 <?php } ?>
 <!-- List Pricing Plan -->
 <?php get_footer(); ?>
