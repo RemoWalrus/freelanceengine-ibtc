3efb56077d9c76353c075301deae30ca802b75bb
Remo Walrus <36936130+RemoWalrus@users.noreply.github.com>
2020-06-03 11:28:16 -0400

    pricing fix logged out

--- page-pricing-IBTC.php
+++ page-pricing-IBTC.php
@@ -129,97 +129,7 @@ if(  is_user_logged_in() ) {
     </div>		
   </div>
 </div>
-<!-- employer Pricing -->
-<div class="fre-service">
-  <div class="container">
-			<h2 id="title_service">
-				Select an Employer Plan</h2>
-			<div class="fre-service-content">
-                <p style="text-align: center; font-size: 1.3em;">Our platform solely caters to an Artist’s creative appetite to find jobs, connect, and be inspired.</p>
-                
-	<div class="row">
-		<div class="col-md-1 hidden-sm"></div>
-		<div class="col-md-10">
-			<div class="row fre-service-package-list">
-					    		<div class="col-md-4 col-sm-6">
-									<div class="fre-service-pricing">
-										<div class="service-price">
-											<h2>FREE</h2> 	
-                                        </div>
-										<div class="service-info">
-											<h3>Basic </h3>
-											<p></p><p>Try us out for free and see what we’re about</p>
-                                         <br>
-                                            
-<p></p>
-                                            
-										</div>
-                                      
-											<p class="freelance-yourplan">Your Plan</p>
-                                        <div class="service-list">
-                                        <ul>
-                                                <li>No Commitments</li>
-                                                <li>Create a profile for your business</li>
-                                                <li>Browse competitive beauty jobs</li>
-                                                <li>Unlimited access to browse our Artists’ profiles</li>
-                                         </ul> 
-                                        </div>
-										
-                                      
-									</div>
-								</div>
-                                <div class="col-md-4 col-sm-6">
-                	            <div class="fre-service-pricing">
-										<div class="service-price">
-											<h2><sup>$</sup>30<sub style="font-size:.3em; ">/post</sub></h2> 										</div>
-										<div class="service-info">
-											<h3>Employer Basic</h3>
-											<p></p><p>Pay as you go Month to Month</p>
-<p></p>
-										</div>
-												<a class="fre-btn primary-bg-color" href="<?php echo get_site_url(); ?>/submit-project/">Upgrade</a>
-                                    
-                                    
-                                        <div class="service-list">
-                                        <ul>
-                                            <li>Create a profile for your business</li>
-                                            <li>Unlimited access to browse our Artists’ profiles</li>
-                                            <li>Post jobs for $30 per unique listings</li>
-                                            <li>Post 1 job active for 30 days</li>
-                                            <li>Renew your existing job listing for $30 if you  need more time to find an Artist</li>
-                                         </ul> 
-                                        </div>
-								</div>
-								</div>
-                                <div class="col-md-4 col-sm-6">
-									<div class="fre-service-pricing">
-										<div class="service-price">
-											<h2><sup>$</sup>400<sub style="font-size:.3em; ">/year</sub></h2> 										</div>
-										<div class="service-info">
-											<h3>Employer Unlimited</h3>
-											<p></p><p>Annual subscription paid in one payment of $400 every year</p>
-<p></p>
-										</div>
-												<a class="fre-btn primary-bg-color" href="<?php echo get_site_url(); ?>/submit-project/">Upgrade</a>
-                                        <div class="service-list">
-                                        <ul>
-                                            <li>Create a profile for your business</li>
-                                            <li>Unlimited access to browse our Artists’ profiles</li>
-                                            <li>Post unlimited jobs, each one stays active for 30 days - no monthly caps!</li>
-                                            <li>Apply to unlimited beauty jobs</li>
-                                            <li>Renew your existing job listing for free if you need more time to find an Artis</li>
-                                         </ul> 
-                                        </div>
-									</div>
-								</div>
-                  </div>
-		  </div>
-		<div class="col-md-1 hidden-sm"></div>
-	</div>
 
-    </div>		
-  </div>
-</div>
 
 <?php
 	} else if(  in_array( $user_role, array( EMPLOYER,'administrator'  ) ) && $pay_to_bid && ! current_user_can('manage_options') ) {
@@ -337,6 +247,99 @@ if(  is_user_logged_in() ) {
     </div>		
   </div>
 </div>
+<!-- employer Pricing -->
+
+<div class="fre-service">
+  <div class="container">
+			<h2 id="title_service">
+				Select an Employer Plan</h2>
+			<div class="fre-service-content">
+                <p style="text-align: center; font-size: 1.3em;">Our platform solely caters to an Artist’s creative appetite to find jobs, connect, and be inspired.</p>
+                
+	<div class="row">
+		<div class="col-md-1 hidden-sm"></div>
+		<div class="col-md-10">
+			<div class="row fre-service-package-list">
+					    		<div class="col-md-4 col-sm-6">
+									<div class="fre-service-pricing">
+										<div class="service-price">
+											<h2>FREE</h2> 	
+                                        </div>
+										<div class="service-info">
+											<h3>Basic </h3>
+											<p></p><p>Try us out for free and see what we’re about</p>
+                                         <br>
+                                            
+<p></p>
+                                            
+										</div>
+                                      
+											<a class="fre-btn primary-bg-color" data-hover="Sign Up" href="<?php echo et_get_page_link('register', array("role"=>'employer')); ?>"><?php _e('<span>Sign Up</span>', ET_DOMAIN);?></a>
+                                        <div class="service-list">
+                                        <ul>
+                                                <li>No Commitments</li>
+                                                <li>Create a profile for your business</li>
+                                                <li>Browse competitive beauty jobs</li>
+                                                <li>Unlimited access to browse our Artists’ profiles</li>
+                                         </ul> 
+                                        </div>
+										
+                                      
+									</div>
+								</div>
+                                <div class="col-md-4 col-sm-6">
+                	            <div class="fre-service-pricing">
+										<div class="service-price">
+											<h2><sup>$</sup>30<sub style="font-size:.3em; ">/post</sub></h2> 										</div>
+										<div class="service-info">
+											<h3>Employer Basic</h3>
+											<p></p><p>Pay as you go Month to Month</p>
+<p></p>
+										</div>
+												<a class="fre-btn primary-bg-color" data-hover="Sign Up" href="<?php echo et_get_page_link('register', array("role"=>'employer')); ?>"><?php _e('<span>Sign Up</span>', ET_DOMAIN);?></a>
+                                    
+                                    
+                                        <div class="service-list">
+                                        <ul>
+                                            <li>Create a profile for your business</li>
+                                            <li>Unlimited access to browse our Artists’ profiles</li>
+                                            <li>Post jobs for $30 per unique listings</li>
+                                            <li>Post 1 job active for 30 days</li>
+                                            <li>Renew your existing job listing for $30 if you  need more time to find an Artist</li>
+                                         </ul> 
+                                        </div>
+								</div>
+								</div>
+                                <div class="col-md-4 col-sm-6">
+									<div class="fre-service-pricing">
+										<div class="service-price">
+											<h2><sup>$</sup>400<sub style="font-size:.3em; ">/year</sub></h2> 										</div>
+										<div class="service-info">
+											<h3>Employer Unlimited</h3>
+											<p></p><p>Annual subscription paid in one payment of $400 every year</p>
+<p></p>
+										</div>
+												<a class="fre-btn primary-bg-color" data-hover="Sign Up" href="<?php echo et_get_page_link('register', array("role"=>'employer')); ?>"><?php _e('<span>Sign Up</span>', ET_DOMAIN);?></a>
+                                        <div class="service-list">
+                                        <ul>
+                                            <li>Create a profile for your business</li>
+                                            <li>Unlimited access to browse our Artists’ profiles</li>
+                                            <li>Post unlimited jobs, each one stays active for 30 days - no monthly caps!</li>
+                                            <li>Apply to unlimited beauty jobs</li>
+                                            <li>Renew your existing job listing for free if you need more time to find an Artis</li>
+                                         </ul> 
+                                        </div>
+									</div>
+								</div>
+                  </div>
+		  </div>
+		<div class="col-md-1 hidden-sm"></div>
+	</div>
+
+    </div>		
+  </div>
+</div>
+
 <?php	} else if( ! $is_post_free ){
 		$show_project_pack = true;
 	}
@@ -434,6 +437,7 @@ if( $show_project_pack || $show_bid_pack ){ ?>
     </div>		
   </div>
 </div>
+
 <?php } ?>
 <!-- List Pricing Plan -->
 <?php get_footer(); ?>
