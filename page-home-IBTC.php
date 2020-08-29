80e3f8cab04fc9563c3096ff03f491c6990241b0
Remo Walrus <36936130+RemoWalrus@users.noreply.github.com>
2020-08-29 03:42:19 -0400

    change homepage button

--- page-home-IBTC.php
+++ page-home-IBTC.php
@@ -182,7 +182,7 @@ global $user_ID;
 			<?php }else{ ?>
 				<?php if(ae_user_role($user_ID) == FREELANCER){ ?>
 					<h2 id="title_start"><?php echo get_theme_mod("title_start_freelancer") ? get_theme_mod("title_start_freelancer") : __("It's time to start finding freelance jobs online!" , ET_DOMAIN);?></h2>
-					<a class="fre-btn primary-bg-color" href="<?php echo get_post_type_archive_link( PROJECT ); ?>"><?php _e('Find Projects', ET_DOMAIN)?></a>
+					<a class="fre-btn primary-bg-color" href="<?php echo get_post_type_archive_link( PROJECT ); ?>"><?php _e('Find Jobs', ET_DOMAIN)?></a>
 				<?php } 
                 } ?>
 
