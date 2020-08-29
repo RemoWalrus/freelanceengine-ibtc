11784efeca3ed3a15bda066501d74f27f80908a5
Remo Walrus <36936130+RemoWalrus@users.noreply.github.com>
2020-08-29 05:38:45 -0400

    Pricing

--- page-pricing-IBTC.php
+++ page-pricing-IBTC.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Template Name: Pricing IBTC Backup
+ * Template Name: Pricing IBTC
  */
 
 get_header();
@@ -37,7 +37,7 @@ if(  is_user_logged_in() ) {
 											<h2>FREE</h2> 	
                                         </div>
 										<div class="service-info">
-											<h3>Basic </h3>
+											<h3>Premier </h3>
 											<p></p><p>Try us out for free and see what we’re about</p>
                                          <br>
                                             
@@ -63,7 +63,7 @@ if(  is_user_logged_in() ) {
 										<div class="service-price">
 											<h2><sup>$</sup>35<sub style="font-size:.3em; ">/month</sub></h2> 										</div>
 										<div class="service-info">
-											<h3>Artist Light</h3>
+											<h3>Popular</h3>
 											<p></p><p>Pay as you go Month to Month</p>
 <p></p>
 										</div>
@@ -84,7 +84,7 @@ if(  is_user_logged_in() ) {
 										<div class="service-price">
 											<h2><sup>$</sup>25<sub style="font-size:.3em; ">/month</sub></h2> 										</div>
 										<div class="service-info">
-											<h3>Artist Standard</h3>
+											<h3>Pro</h3>
 											<p></p><p>28% Savings Six month subscription paid in one payment of $150 every 6 months</p>
 <p></p>
 										</div>
@@ -105,7 +105,7 @@ if(  is_user_logged_in() ) {
 										<div class="service-price">
 											<h2><sup>$</sup>15<sub style="font-size:.3em; ">/month</sub></h2> 										</div>
 										<div class="service-info">
-											<h3>Artist Premium</h3>
+											<h3>Premium</h3>
 											<p></p><p>57% Savings Annual subscription paid in one payment of $180 every year</p>
 <p></p>
 										</div>
@@ -138,21 +138,9 @@ if(  is_user_logged_in() ) {
 
 } else { // visitor.
 	if( $pay_to_bid ){ ?>
-<div class="fre-page-wrapper">
-<div class="fre-page-title">
- <ul class="fre-tabs nav-tabs-my-work">
-                        <li class="active"><a data-toggle="tab"
-                             href="#artist-pricing-tab"><span><?php _e( 'Artists', ET_DOMAIN ); ?></span></a>
-                        </li>
-                        <li class="next"><a data-toggle="tab"
-                            href="#employer-pricing-tab"><span><?php _e( 'Employers', ET_DOMAIN ); ?></span></a>
-                        </li>
-                   </ul>
-   </div> 
-    
-<div class="fre-tab-content fre-service logout">
- <div id="artist-pricing-tab" class="freelancer-current-project-tab fre-panel-tab active">     
-   <div class="container">
+
+<div class="fre-service">
+      <div class="container">
 			<h2 id="title_service">
 				Select an Artist Plan</h2>
 			<div class="fre-service-content">
@@ -168,7 +156,7 @@ if(  is_user_logged_in() ) {
 											<h2>FREE</h2> 	
                                         </div>
 										<div class="service-info">
-											<h3>Artist Basic </h3>
+											<h3>Premier </h3>
 											<p></p><p>Try us out for free and see what we’re about</p>
                                          <br>
                                             
@@ -194,7 +182,7 @@ if(  is_user_logged_in() ) {
 										<div class="service-price">
 											<h2><sup>$</sup>35<sub style="font-size:.3em; ">/month</sub></h2> 										</div>
 										<div class="service-info">
-											<h3>Artist Light</h3>
+											<h3>Popular</h3>
 											<p></p><p>Pay as you go Month to Month</p>
 <p></p>
 										</div>
@@ -215,7 +203,7 @@ if(  is_user_logged_in() ) {
 										<div class="service-price">
 											<h2><sup>$</sup>25<sub style="font-size:.3em; ">/month</sub></h2> 										</div>
 										<div class="service-info">
-											<h3>Artist Standard</h3>
+											<h3>Pro</h3>
 											<p></p><p>28% Savings Six month subscription paid in one payment of $150 every 6 months</p>
 <p></p>
 										</div>
@@ -236,7 +224,7 @@ if(  is_user_logged_in() ) {
 										<div class="service-price">
 											<h2><sup>$</sup>15<sub style="font-size:.3em; ">/month</sub></h2> 										</div>
 										<div class="service-info">
-											<h3>Artist Premium</h3>
+											<h3>Premium</h3>
 											<p></p><p>57% Savings Annual subscription paid in one payment of $180 every year</p>
 <p></p>
 										</div>
@@ -259,101 +247,8 @@ if(  is_user_logged_in() ) {
 
     </div>		
   </div>
- </div>         
-
-<!-- employer Pricing -->
- <div id="employer-pricing-tab" class="freelancer-current-project-tab fre-panel-tab">       
-  <div class="container">
-			<h2 id="title_service">
-				Select an Employer Plan</h2>
-			<div class="fre-service-content">
-                <p style="text-align: center; font-size: 1.3em;">Our platform solely caters to an Artist’s creative appetite to find jobs, connect, and be inspired.</p>
-                
-	<div class="row">
-		<div class="col-md-1 hidden-sm"></div>
-		<div class="col-md-10">
-			<div class="row fre-service-package-list">
-					    		<div class="col-md-4 col-sm-6">
-									<div class="fre-service-pricing">
-										<div class="service-price">
-											<h2>FREE</h2> 	
-                                        </div>
-										<div class="service-info">
-											<h3>Employer Basic </h3>
-											<p></p><p>Try us out for free and see what we’re about</p>
-                                         <br>
-                                            
-<p></p>
-                                            
-										</div>
-                                      
-											<a class="fre-btn primary-bg-color" data-hover="Sign Up" href="<?php echo et_get_page_link('register', array("role"=>'employer')); ?>"><?php _e('<span>Sign Up</span>', ET_DOMAIN);?></a>
-                                        <div class="service-list">
-                                        <ul>
-                                                <li>No Commitments</li>
-                                                <li>Create a profile for your business</li>
-                                                <li>Browse competitive beauty jobs</li>
-                                                <li>Unlimited access to browse our Artists’ profiles</li>
-                                         </ul> 
-                                        </div>
-										
-                                      
-									</div>
-								</div>
-                                <div class="col-md-4 col-sm-6">
-                	            <div class="fre-service-pricing">
-										<div class="service-price">
-											<h2><sup>$</sup>30<sub style="font-size:.3em; ">/post</sub></h2> 										</div>
-										<div class="service-info">
-											<h3>Employer Premium</h3>
-											<p></p><p>Pay as you go Month to Month</p>
-<p></p>
-										</div>
-												<a class="fre-btn primary-bg-color" data-hover="Sign Up" href="<?php echo et_get_page_link('register', array("role"=>'employer')); ?>"><?php _e('<span>Sign Up</span>', ET_DOMAIN);?></a>
-                                    
-                                    
-                                        <div class="service-list">
-                                        <ul>
-                                            <li>Create a profile for your business</li>
-                                            <li>Unlimited access to browse our Artists’ profiles</li>
-                                            <li>Post jobs for $30 per unique listing</li>
-                                            <li>Post 1 job active for 30 days</li>
-                                            <li>Renew your existing job listing for $30 if you  need more time to find an Artist</li>
-                                         </ul> 
-                                        </div>
-								</div>
-								</div>
-                                <div class="col-md-4 col-sm-6">
-									<div class="fre-service-pricing">
-										<div class="service-price">
-											<h2><sup>$</sup>400<sub style="font-size:.3em; ">/year</sub></h2> 										</div>
-										<div class="service-info">
-											<h3>Employer Unlimited</h3>
-											<p></p><p>Annual subscription paid in one payment of $400 every year</p>
-<p></p>
-										</div>
-												<a class="fre-btn primary-bg-color" data-hover="Sign Up" href="<?php echo et_get_page_link('register', array("role"=>'employer')); ?>"><?php _e('<span>Sign Up</span>', ET_DOMAIN);?></a>
-                                        <div class="service-list">
-                                        <ul>
-                                            <li>Create a profile for your business</li>
-                                            <li>Unlimited access to browse our Artists’ profiles</li>
-                                            <li>Post unlimited jobs, each one stays active for 30 days - no monthly caps!</li>
-                                            <li>Apply to unlimited beauty jobs</li>
-                                            <li>Renew your existing job listing for free if you need more time to find an Artist</li>
-                                         </ul> 
-                                        </div>
-									</div>
-								</div>
-                  </div>
-		  </div>
-		<div class="col-md-1 hidden-sm"></div>
-	</div>
-
-    </div>		
-  </div>
-</div>        
 </div>
-</div>
+
 
 <?php	} else if( ! $is_post_free ){
 		$show_project_pack = true;
@@ -405,7 +300,7 @@ if( $show_project_pack || $show_bid_pack ){ ?>
 										<div class="service-price">
 											<h2><sup>$</sup>30<sub style="font-size:.3em; ">/post</sub></h2> 										</div>
 										<div class="service-info">
-											<h3>Employer Basic</h3>
+											<h3>Employer Premium</h3>
 											<p></p><p>Pay as you go Month to Month</p>
 <p></p>
 										</div>
