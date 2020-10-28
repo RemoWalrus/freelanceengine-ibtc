297894e14dc3c3f5021334564878e350c5ada640
Remo Walrus <36936130+RemoWalrus@users.noreply.github.com>
2020-10-27 22:35:51 -0400

    add counter to homepage

--- page-home-IBTC - Coming Soon.php
+++ page-home-IBTC - Coming Soon.php
@@ -19,6 +19,7 @@ global $user_ID;
 					<img class="logo" src="<?php echo get_site_url(); ?>/wp-content/themes/freelanceengine-ibtc/assets/images/inbetweelogo_logo.svg">
                     <br>
                     <img class="comingsoon" src="<?php echo get_site_url(); ?>/wp-content/themes/freelanceengine-ibtc/assets/images/comingsoon.svg">
+					<?php if( dynamic_sidebar('counter_homepage_area') ) : else : endif; ?>
 			
 					<a class="fre-btn primary-bg-color" href="<?php echo get_post_type_archive_link( PROFILE ); ?>"><?php _e('Find Artist', ET_DOMAIN);?></a>
 					<a class="fre-btn primary-bg-color" href="<?php echo get_post_type_archive_link( PROJECT ); ?>"><?php _e('Find Jobs', ET_DOMAIN);?></a>
